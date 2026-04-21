0572866c39f4697bd66e994bdb2dfa673479cb6c
RAFAEL GRACIA <djrafael1004@gmail.com>
2026-04-21 14:54:18 -0500

    He añadido usePathname a la importación desde next/navigation en layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { Sidebar } from '@/components/layout/Sidebar';
 import { getAuthToken, clearAuthCookie, isTokenExpired } from '@/utils/auth';
 import styles from './DashboardLayout.module.css';
