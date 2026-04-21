3533345f4799bd7c8690473b1ffa0bc701453210
RAFAEL GRACIA <djrafael1004@gmail.com>
2026-04-21 14:25:45 -0500

    se areglo asistencia

--- frontend-colegio/.next/dev/static/chunks/frontend-colegio_src_app_favicon_ico_mjs_c5345a24._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/94bc8_next_dist_76be2739._.js"
-],
-    source: "dynamic"
-});
--- frontend-colegio/.next/dev/static/chunks/frontend-colegio_src_app_layout_tsx_cbf0fcd8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend-colegio_src_app_globals_6c6e9117.css",
-  "static/chunks/frontend-colegio_ba165279._.js"
-],
-    source: "dynamic"
-});
